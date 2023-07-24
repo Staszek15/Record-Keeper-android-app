64633c500b918646f8d7751bb036b941d633da2f
Mateusz Stasiak <mateusz.stasiak15@wp.pl>
2023-07-24 10:27:45 +0200

    first push

---
+++ app/src/main/java/com/staszek15/recordkeeper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.staszek15.recordkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
