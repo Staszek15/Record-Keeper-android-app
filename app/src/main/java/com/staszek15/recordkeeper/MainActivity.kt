714edbacc67691a4ff991f4146a537b7386cb80a
Mateusz Stasiak <mateusz.stasiak15@wp.pl>
2023-07-25 13:25:33 +0200

    running and cycling layouts

--- app/src/main/java/com/staszek15/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/staszek15/recordkeeper/MainActivity.kt
@@ -2,10 +2,56 @@ package com.staszek15.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
+import com.staszek15.recordkeeper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    private fun onRunningClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+    }
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+    private fun onSwimmingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, SwimmingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_running -> {
+                onRunningClicked()
+                return true
+            }
+            R.id.nav_cycling -> {
+                onCyclingClicked()
+                return true
+            }
+            R.id.nav_swimming -> {
+                onSwimmingClicked()
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
     }
 }
